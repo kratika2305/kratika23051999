d68d952d03d24706c60446c426a48e4e1eb3969d
kratika2305 <44507327+kratika2305@users.noreply.github.com>
2018-11-20 01:22:38 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class count 
+{
+   public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter N");
+        int n= sc.nextInt();
+        int c=0;
+        while(n>0)
+        {
+        
+            n=n/10;
+            c++; 
+       
+        }
+            System.out.println(c);
+        
+      
+    } 
+}
