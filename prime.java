f47b1a1c9c5a537f5b278c764ce426d6ea37d061
kratika2305 <44507327+kratika2305@users.noreply.github.com>
2018-11-20 11:52:11 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class prime 
+{
+     public static void main(String[] args) 
+    {Scanner sc = new Scanner(System.in);
+        System.out.println("enter N<=1000");
+        int n= sc.nextInt();
+        int f=0;
+        for(int i=2;i<=n/2;i++)
+        {
+        
+        if (n%i==0)
+        {
+             f=1;
+        }
+        }
+        if(f==1)
+        {
+            System.out.println("no");
+        }
+        else
+        {
+            System.out.println("yes");
+        }
+       
+    }
+    
+    }
+
