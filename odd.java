9bcc62c1caf57c0d9b023bff58bcd330dda81a0c
kratika2305 <44507327+kratika2305@users.noreply.github.com>
2018-11-20 12:35:44 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class odd 
+{
+     public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter N <=1000");
+        int n= sc.nextInt();
+         System.out.println("enter Q<=1000");
+        int q = sc.nextInt();
+        for(int i=n ; i<q ; i++)
+        {
+          if(i%2!=0 && i!=n)
+          {
+           System.out.println(i+" ");
+          }
+        }
+         System.out.println();
+}
+    
+}
