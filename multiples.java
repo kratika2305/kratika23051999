25a737c6b547f4b668fec61317d521487d90d944
kratika2305 <44507327+kratika2305@users.noreply.github.com>
2018-11-23 13:22:57 +0530

    Add files via upload

---
+++ multiples.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class multiples 
+{
+     public static void main(String[] args) 
+    {Scanner sc = new Scanner(System.in);
+        System.out.println("enter N<=1000");
+        int n= sc.nextInt();
+        int f=1;
+        for(int i=1;i<=5;i++)
+        {
+            f=n*i;
+        System.out.print(f+" ");
+        
+        }
+        
+      
+
+      
+    }
+    
+}
