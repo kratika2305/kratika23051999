5f11afcceb619398b6a8ef99e23136ba3563c7b1
kratika2305 <44507327+kratika2305@users.noreply.github.com>
2019-01-30 22:47:46 +0530

    Add files via upload

---
+++ numeric_count.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class numeric_count {
+    
+public static void main(String[] args)
+ {
+     Scanner sc = new Scanner(System.in);
+     
+      System.out.println("enter string");
+        String input=sc.nextLine();
+        int c=0;
+        for(int i=0;i<input.length();i++)
+        {
+           char ch=input.charAt(i);
+           int val=(int)ch;
+           if(val>=48 && val<=57)
+           {
+               c++;
+           }
+           
+        }
+         System.out.println(c);
+ }
+}
+
