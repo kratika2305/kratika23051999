6a1b5ca179f20adc8e71baaf3a61cb48b7d71fd4
kratika2305 <44507327+kratika2305@users.noreply.github.com>
2019-02-21 08:29:16 +0530

    Add files via upload

---
+++ next_no.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class next_no 
+{
+     public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter n");
+        int n = sc.nextInt();
+       
+        System.out.println(n+1);
+    }
+    
+}
