3a0126aeaee3f559f41e3fb22da553bd97590174
kratika2305 <44507327+kratika2305@users.noreply.github.com>
2019-02-21 08:10:28 +0530

    Add files via upload

---
+++ inclusive.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class inclusive 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n");
+        double n =sc.nextDouble();
+        if(n>=1 && n<=10)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+    
+}
