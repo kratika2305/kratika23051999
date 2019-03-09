6f572c26d629ca8686dc1dfd9a52c7cd7789bea5
kratika2305 <44507327+kratika2305@users.noreply.github.com>
2019-03-09 23:26:44 +0530

    Add files via upload

---
+++ without_semicolon.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guvi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class without_semicolon 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("enter string");
+        String s =sc.nextLine();
+      if(System.out.printf(s)==null)
+      {
+          
+      }
+    }
+    
+}
